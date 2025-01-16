9eded679c577c6624bd49021d0d6ed5a901d6a30
desarrollador1 <desarrollador1@DESARROLLO_HP80>
2025-01-16 13:02:17 -0600

    Agregar archivos de proyecto.

---
+++ Ex-QUID/Ribbon.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Office = Microsoft.Office.Core;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace Ex_QUID
+{
+    [ComVisible(true)]
+    public class Ribbon : Office.IRibbonExtensibility
+    {
+        public string GetCustomUI(string ribbonID)
+        {
+            return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><customUI xmlns=\"http://schemas.microsoft.com/office/2009/07/customui\"><ribbon><tabs><tab id=\"customTab\" label=\"Ex-QUID\"><group id=\"connectionGroup\" label=\"Connection\"><button id=\"button1\" label=\"Botón 1\" size=\"large\" onAction=\"OnButtonClick\" /></group><group id=\"group2\" label=\"Grupo 2\"><button id=\"button2\" label=\"Botón 2\" size=\"large\" onAction=\"OnButtonClick\" /></group><group id=\"group3\" label=\"Grupo 3\"><button id=\"button3\" label=\"Botón 3\" size=\"large\" onAction=\"OnButtonClick\" /></group><group id=\"group4\" label=\"Grupo 4\"><button id=\"button4\" label=\"Botón 4\" size=\"large\" onAction=\"OnButtonClick\" /></group><group id=\"group5\" label=\"Grupo 5\"><button id=\"button5\" label=\"Botón 5\" size=\"large\" onAction=\"OnButtonClick\" /></group></tab></tabs></ribbon></customUI>";
+        }
+
+        #region Connection types (SQL Server and MySQL)
+        public SqlConnection ConnectToSqlServer()
+        {
+            string connectionString = "Server=localhost;Database=YourDatabaseName;Trusted_Connection=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de conexión: " + ex.Message);
+                return null;
+            }
+        }
+
+        public MySqlConnection ConnectToMySql()
+        {
+            string connectionString = "Server=localhost;Database=YourDatabaseName;Uid=YourUsername;Pwd=YourPassword;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de conexión: " + ex.Message);
+                return null;
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ Ex-QUID/ThisAddIn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+namespace Ex_QUID
+{
+    public partial class ThisAddIn
+    {
+        private Ribbon ribbon;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        protected override Office.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            ribbon = new Ribbon();
+            return ribbon;
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+    }
+}
